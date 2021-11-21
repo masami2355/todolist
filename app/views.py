47e2d6a4f65cea3691133191a53231a6a08564e4
masami <masami@syks@theia.ocn.ne.jp>
2021-11-21 21:07:45 +0900

    git commit - m

--- app/views.py
+++ app/views.py
@@ -1,52 +1,102 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
 from .models import Task
+from django.views import View
+from django.db import transaction
 
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from .forms import PositionForm
 
+#ログインページ
 class CustomLoginView(LoginView):
-    template_naem = 'app/login.html'
+    template_name = 'registration/login.html'
     fields = '__all__'
     redirect_authenticated_user = True
      
     def get_success_url(self):
         return reverse_lazy('tasks')
 
+#ユーザー登録ページ
+class RegisterPage(FormView):
+    template_name = 'app/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(RegisterPage, self).get(*args, **kwargs)
+
+#タスク一覧ページ
 class TaskList(LoginRequiredMixin, ListView):
     model = Task
-    context_ocject_name = 'tasks'
+    context_object_name = 'tasks'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
         context['count'] = context['tasks'].filter(complete=False).count()
+
+        #検索フォーム
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(title__contains=search_input)
+
+        context['search_input'] = search_input
         return context
 
+#詳細ページ
 class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_ocject_name = 'task'
     template_name = 'app/task_detail.html'
 
+#タスク登録フォーム
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
     fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
 
-    def form_invalid(self, form):
+    def form_valid(self, form):
         form.instance.user = self.request.user
         return super(TaskCreate, self).form_valid(form)
 
+#タスク編集フォーム
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
     fields = ['title', 'description', 'complete'] #'__all__'
     success_url = reverse_lazy('tasks')
 
+
+#タスク削除
 class DeleteView(LoginRequiredMixin, DeleteView):
     model = Task
     context_object_name = 'task'
     success_url = reverse_lazy('tasks')
+
+
+class TaskReorder(View):
+    def post(self, request):
+        form = PositionForm(request.POST)
+
+        if form.is_valid():
+            positionList = form.cleaned_data["position"].split(',')
+
+            with transaction.atomic():
+                self.request.user.set_task_order(positionList)
+
+        return redirect(reverse_lazy('tasks'))
